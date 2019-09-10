e9e9bb56083abe3d729e062ef2d715c983d54f1d
ricardofcj <ricardofcj@gmail.com>
2019-09-10 15:39:54 -0300

    first commit

---
+++ app.R
@@ -0,0 +1,112 @@
+library(data.table);library(magrittr);library(ggplot2);library(shiny);library(readxl)
+library(shinythemes);library(lubridate);library(plotly);library(shinydashboard)
+source("/home/ricardo/geicdatamining/coletando.R")
+source("/home/ricardo/geicdatamining/functions.R")
+source('functions.R')
+
+ui = dashboardPage(
+  dashboardHeader(title='GEIC Alunos'),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem('Aluno',tabName='tab_aluno'),
+      numericInput('idaluno',"ID do aluno",value=0),
+      actionButton('search_aluno','Buscar')
+      )
+    ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = 'tab_aluno',
+              box(width=12,status='info',htmlOutput('alunoinfo')),
+              box(width=12,title='Filtros',collapsible = T,collapsed = T,
+                  column(12,
+                         column(5,textInput('st_date','Data inicial',placeholder = 'DD-MM-YYYY')),
+                         column(5,textInput('en_date','Data final',placeholder = 'DD-MM-YYYY')),
+                         column(2,actionButton('update_data','Atualizar'))),
+                  column(12,selectInput('programa','Programa',choices=c()))),
+              column(12,plotlyOutput('graph1')),column(12,imageOutput('graph2')),
+              column(12,column(6,plotlyOutput('graph3')),column(6,plotlyOutput('graph4')))
+              )
+    )
+  )
+)
+vars=reactiveValues(dados=NULL)
+server=function(input,output,session){
+  observeEvent(input$search_aluno,{
+    vars$dados=process(takeit.byalunoSQL(input$idaluno))
+    vars$dados[,Week:=week(Data)]
+    vars$dados[,YWeek:=paste(ANO.INICIO.Sess,Week,sep='-')]
+    availProgs = vars$dados[,sort(as.character(unique(PROGRAMA)))]
+    progs=sort(dir('programas'))
+    progsNam=sapply(progs,
+                    function(x){
+                      unlist(strsplit(x,'-'))%>%{paste0(unlist(strsplit(.[2],'\\.')[1])[1],' (',.[1],')')}
+                    })
+    progs=sapply(progs,function(x)unlist(strsplit(x,'-'))[1])
+    progs=intersect(availProgs,progs)
+    names(progs)=grep(paste(progs,collapse="|"),progsNam,value=T)
+    updateSelectInput(session,'programa',choices=progs)
+  })
+  
+  observeEvent(c(vars$dados,input$update_data),{
+    if(!is.null(vars$dados)){
+      dados=vars$dados
+      maxD=as.Date(Sys.Date())
+      minD=dados[,min(Data)]
+      if(!input$st_date==''){
+        stdate = input$st_date%>%{unlist(strsplit(.,'-'))}%>%{paste(.[3],.[2],.[1],sep='-')}
+        dados=dados[Data>=as.Date(stdate)]
+        updateTextInput(session,'st_date',value='')
+        minD=as.Date(stdate)
+        }
+      if(!input$en_date==''){
+        endate = input$en_date%>%{unlist(strsplit(.,'-'))}%>%{paste(.[3],.[2],.[1],sep='-')}
+        dados=dados[Data<=as.Date(endate)]
+        updateTextInput(session,'en_date',value='')
+        maxD=as.Date(endate)
+        }
+      vals=dados[,.(uniqueN(Data)),by=YWeek]
+      dt=data.table(dates=seq(minD,maxD,by=1))
+      dt[,ano:=substring(dates,1,4)]
+      dt[,week:=week(dates)]
+      dt[,ydate:=as.factor(paste(ano,week,sep='-'))]
+      dt=dt[,max(dates),by=ydate]
+      sdt=data.table(YWeek=dt[!ydate%in%vals,ydate],V1=rep(0,dt[!ydate%in%vals,.N]))
+      vals=rbindlist(list(vals,sdt))
+      vals[,YWeek:=factor(YWeek,levels=levels(dt$ydate))]
+      
+      output$alunoinfo = renderUI(HTML(paste(alunoinfo(dados),collapse='<br>')))
+      output$graph1=renderPlotly(
+        print(ggplotly(
+        ggplot(vals,aes(x=YWeek,y=V1))+geom_bar(stat='identity')+labs(x='Semanas do ano',y="Dias frequentados")+
+          scale_x_discrete(breaks=dt$ydate,labels=format.Date(dt$V1,'%d-%m-%Y'))+theme_bw()+theme(axis.text.x = element_text(angle=45,hjust=1))+
+          coord_cartesian(ylim=c(0,5))
+      )))
+      output$graph2=renderImage({
+        inp.progr=input$programa
+        nome.arq=grep(inp.progr,dir('programas'),value=T)
+        progr = ifelse(length(nome.arq)!=0,paste0('programas/',nome.arq),0)
+        outfile <- tempfile(fileext = ".png")
+        width  <- session$clientData$output_graph2_width
+        height <- session$clientData$output_graph2_height
+        png(outfile, width=width, height=height)
+        if(is.numeric(progr)){
+          plot(c(0,200),c(0,100),ann=F,xaxt='n',yaxt='n',bty='n',col='white')
+          text(100,50,labels='Programa\nindisponível',cex=3)
+        }else{
+          drawProg(progr,vars$dados[PROGRAMA%in%as.numeric(input$programa)&CANCELADA==0&OCORRENCIA_N==1])
+        }
+        # Return a list containing information about the image
+        dev.off()
+        list(
+          src = outfile,
+          contentType = "image/png",
+          width = width,
+          height = height,
+          alt='Caminho'
+        )
+      })
+    }
+  })
+}
+
+shinyApp(ui,server)
---
+++ functions.R
@@ -0,0 +1,49 @@
+#GEIC Alunos' functions
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+alunoinfo = function(dt){
+  nome = paste0('Aluno: ',dt[,unique(NOME)])
+  escola = unlist(dbGetQuery(sql,paste0('select NOME from ESCOLA where ID in (',dt[,unique(ESCOLA)],');')))
+  Encoding(escola)='latin1'
+  escola = paste0('Escola: ',escola)
+  programas = paste0('Programas: ',paste0(dt[,unique(PROGRAMA)],collapse=', '))
+  dias = paste0('Dias frequentados: ',dt[,uniqueN(Data)])
+  sess = paste0('Sessões executadas: ',dt[,uniqueN(SESSAOEXEC_ID)])
+  c(nome,escola,programas,dias,sess)
+}
+dateF = function(x){format.Date(x,"%d de %b de %Y")}
+
+drawProg=function(path,al){
+  programa=unlist(strsplit(path,"-"))[[1]][1]%>%{gsub("[^0-9]","",.)}%>%as.numeric
+  prog=as.data.table(read_xlsx(path))
+  passos=prog[,PASSO]
+  passoOco = as.data.table(dbGetQuery(sql,paste0('select * from PASSOOCORRENCIA where PASSO_ID in (',paste(passos,collapse=', '),')')))
+  passoOco = passoOco[PROGRAMA_ID==programa]
+  passoOco[,NOME:=factor(prog[match(PASSO_ID,prog$PASSO),NOME],levels=prog[,unique(NOME)])]
+  blocoOco=as.data.table(dbGetQuery(sql,paste0('select * from BLOCOOCORRENCIA where PASSO_ID in (',paste(passoOco[,unique(PASSO_ID)],collapse=', '),')')))
+  bloco=as.data.table(dbGetQuery(sql,paste0('select * from BLOCO where ID in (',paste(blocoOco[,BLOCO_ID],collapse=', '),')')))[,.(ID,TENTATIVAOCORRENCIAINICIAL_ID)]
+  bloco[,PASSO_ID:=blocoOco[match(bloco$ID,BLOCO_ID),PASSO_ID]]
+  passoOco[,TentIni:=bloco[match(passoOco$PASSO_ID,bloco$PASSO_ID),TENTATIVAOCORRENCIAINICIAL_ID]]
+  
+  alt=200;larg=100
+  par(mar=c(0,0,0,0))
+  graphmax=passoOco[,uniqueN(NOME)]*22+25
+  plot(c(-75,1275),c(-75,graphmax),ann=F,xaxt='n',yaxt='n',bty='n',col='white')
+  x=0;y=graphmax-25
+  plts=0
+  for(i in 1:passoOco[,uniqueN(NOME)]){
+    cur.nome=passoOco[NOME%in%levels(NOME)[i],unique(NOME)]
+    cur.passosOco=passoOco[NOME%in%levels(NOME)[i],unique(ID)]
+    cur.passos=passoOco[NOME%in%levels(NOME)[i],unique(PASSO_ID)]
+    symbols(x,y,rectangles = matrix(c(alt,larg),ncol=2),add = T,inches=F,bg=ifelse(al[,any(PASSOOCO_ID%in%cur.passosOco)],'lightgreen','white'))
+    reps=al[PASSOOCO_ID%in%cur.passosOco&OCORRENCIA_ID%in%passoOco[PASSO_ID%in%cur.passos,TentIni],uniqueN(ID),by=OCORRENCIA_ID][,ifelse(.N>0,max(V1),0)]
+    text(x,y,labels=paste0(cur.nome,' (',reps,')'),cex=.7)
+    plts=plts+1
+    if(plts==7){plts=0;x=0;y=y-150}else{
+      x=x+200
+    }
+  }
+  
+}
